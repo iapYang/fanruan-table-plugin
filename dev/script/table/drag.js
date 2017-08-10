e975604c45ba4ff3d9b7f62256ee587649d849c2
DESKTOP-8IQMI96\Creasy <yklorse@gmail.com>
2017-08-10 14:41:08 +0800

    u

--- dev/script/table/drag.js
+++ dev/script/table/drag.js
@@ -2,6 +2,31 @@ import $ from 'jquery';
 
 export default class {
     constructor(selector) {
-        this.$container = $(selector);
+        this.$td = $(selector);
+        this.$tr = this.$td.parent();
+        this.$drag = this.$td.find('.drag');
+
+        this.isResizing = false;
+        this.lastDownX = 0;
+
+        this.addEventListener();
+    }
+    addEventListener() {
+        this.$drag.on('mousedown', e => {
+            this.isResizing = true;
+            this.lastDownX = e.clientX;
+            console.log('mousedown');
+            e.preventDefault();
+        });
+
+        $(document)
+            .on('mousemove', e => {
+                if (this.isResizing) {
+                    console.log('23333');
+                }
+            })
+            .on('mouseup', () => {
+                this.isResizing = false;
+            });
     }
 }
--- dev/script/table/index.js
+++ dev/script/table/index.js
@@ -1,15 +1,14 @@
 import $ from 'jquery';
 
+import Drag from './drag';
+
 export default class {
     constructor(selector, options) {
-        console.log(564);
         this.data = options.data;
         this.rowHeader = options.rowHeader || false;
         this.colHeader = options.colHeader || false;
 
         this.$container = $(selector);
-
-        console.log(this.data);
 
         this.$table = this.createTable();
         this.$container.append(this.$table);
@@ -22,19 +21,28 @@ export default class {
             const $tr = $('<tr></tr>');
             for (let j = 0; j < this.data[i].length; j++) {
                 const $td = $(`
-                    <td>
-                        ${this.data[i][j]}
-                        <div class="drag" data-flag="false">
-                            <img src="${require('../../image/triangle_16px_1103136_easyicon.net.png')}" />
-                        </div>
-                    </td>
+                    <td>${this.data[i][j]}</td>
                 `);
+                const $drag = $(`
+                    <div class="drag">
+                        <img src="${require('../../image/triangle_16px_1103136_easyicon.net.png')}" />
+                    </div>
+                `);
+
+                $td.data('row', i).data('col', j);
+
                 if (i === 0 && this.rowHeader) {
                     $td.addClass('header-td');
                 }
                 if (j === 0 && this.colHeader) {
                     $td.addClass('header-td');
                 }
+
+                if (!$td.hasClass('header-td')) {
+                    $td.append($drag);
+                    new Drag($td);
+                }
+
                 $tr.append($td);
             }
             $table.append($tr);
@@ -44,7 +52,6 @@ export default class {
     }
     tdEventListener() {
         const $td = this.$table.find('td');
-        const $drag = this.$table.find('.drag');
 
         $td.on('click', e => {
             const $item = $(e.currentTarget);
@@ -55,33 +62,5 @@ export default class {
                 $item.addClass('selected');
             }
         });
-
-        console.log($drag);
-
-        $drag
-            .on('mousedown', e => {
-                const $item = $(e.currentTarget);
-                $item.data('flag', true);
-
-                e.preventDefault();
-
-                return false;
-            })
-            .on('mousemove', e => {
-                const $item = $(e.currentTarget);
-                const $parentTd = $item.parent();
-                const $parentTr = $parentTd.parent();
-
-                if (!$item.data('flag')) return;
-                const $allTd = $parentTr.find('td');
-                $allTd.css({
-                    height: Math.abs($parentTd.offset().top - e.pageY) + 3,
-                });
-            })
-            .on('mouseleave', e => {
-                const $item = $(e.currentTarget);
-                const $parent = $item.parent();
-                $item.data('flag', false);
-            });
     }
 }
